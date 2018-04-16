369eb5ead15444977bb70d792b24fe0d62f2b44a
Guilherme Luis <guile.hm@hotmail.com>
2018-04-16 10:00:06 -0300

    Printing colors

--- bot.py
+++ bot.py
@@ -7,5 +7,11 @@ player_color = (83, 83, 83)
 
 print('starting at 4 seconds...')
 time.sleep(4)
-screen = ImageGrab.grab()
 
+def capture_screen():
+    screen = ImageGrab.grab()
+    color = screen.getpixel((520, 415))
+    print(color)
+
+while True:
+    capture_screen()
