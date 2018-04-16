5baae35966327b53234e5742ddac3c678a198c25
Guilherme Luis <guile.hm@hotmail.com>
2018-04-16 11:26:26 -0300

    Improvements

--- bot.py
+++ bot.py
@@ -9,18 +9,17 @@ player_color = (83, 83, 83)
 print('starting at 4 seconds...')
 time.sleep(4)
 
-X = 300.0
+X = 360.0
 
 def detect_object(x1, x2, y1, y2):
     screen = ImageGrab.grab()
-    for x in range(x1, x2):
+    for x in range(int(x1), int(x2)):
         for y in range(y1, y2):
             color = screen.getpixel((x, y))
-            if color == (83, 83, 83):
+            if color == (83, 83, 83) or color == (172, 172, 172):
                 return True
 
 while True:
-    global X
     if detect_object(X, X+20, 390, 450):
         pyautogui.press('up')
-        X += 0.4
+        X += 0.45
