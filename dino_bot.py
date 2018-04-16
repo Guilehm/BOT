dfb8f145a3a10d760ca86c51b1878198d2708f04
Guilherme Luis <guile.hm@hotmail.com>
2018-04-16 19:15:40 -0300

    Tons of Improvements

--- dino_bot.py
+++ dino_bot.py
@@ -3,24 +3,29 @@ import time
 import pyautogui
 from PIL import ImageGrab
 
-bg_color = (247, 247, 247)
+# bg_color = (247, 247, 247)
 player_color = (83, 83, 83)
 
 print('starting at 4 seconds...')
 time.sleep(4)
 
-X = 320.0
+
+
+X = 330.0
 
 def detect_object(x1, x2, y1, y2):
     screen = ImageGrab.grab()
+    bg_color = screen.getpixel((10, 80))
     for x in range(int(x1), int(x2)):
         for y in range(y1, y2):
             color = screen.getpixel((x, y))
-            if color == (83, 83, 83) or color == (172, 172, 172):
+            if color != bg_color:
+                print(color)
                 return True
 
 while True:
-    if detect_object(X, X+20, 390, 450):
+    if detect_object(X, X+20, 380, 420):
         pyautogui.press('up')
-        pyautogui.press('up')
-        X += 0.6
+        seconds = time.clock()
+        # print('Time:', seconds)
+        X += 0.55
