90b9dcff41b4cb48479dd84f20056e9e7c82f19b
Guilherme Luis <guile.hm@hotmail.com>
2018-04-16 10:47:06 -0300

    Working

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import time
 
+import pyautogui
 from PIL import ImageGrab
 
 bg_color = (247, 247, 247)
@@ -8,10 +9,24 @@ player_color = (83, 83, 83)
 print('starting at 4 seconds...')
 time.sleep(4)
 
-def capture_screen():
+# while True:
+#     screen = ImageGrab.grab()
+#     for x in range(300, 350):
+#         for y in range(383, 460):
+#             color = screen.getpixel((x, y))
+#             if color != (247, 247, 247):
+#                 print(color)
+    # monster = screen.getpixel((280, 450))
+    # print(monster)
+
+def detect_object():
     screen = ImageGrab.grab()
-    color = screen.getpixel((520, 415))
-    print(color)
+    for x in range(300, 320):
+        for y in range(390, 450):
+            color = screen.getpixel((x, y))
+            if color == (83, 83, 83):
+                return True
 
 while True:
-    capture_screen()
+    if detect_object():
+        pyautogui.press('up')
