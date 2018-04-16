20437f3585ccf0a21f3d3930d73d3d2a01c816d8
Guilherme Luis <guile.hm@hotmail.com>
2018-04-16 09:54:07 -0300

    Colors defined

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import time
 
 from PIL import ImageGrab
 
+bg_color = (247, 247, 247)
+player_color = (83, 83, 83)
+
 print('starting at 4 seconds...')
 time.sleep(4)
 screen = ImageGrab.grab()
-color = screen.getpixel((100, 430))
-print(color)
+
