cc299e5949a1a5ae2cd861d285bdbf2ba1a2381b
guile.hm <guile.hm@hotmail.com>
2018-04-19 12:54:32 -0300

    Performance improvements

--- dino_bot.py
+++ dino_bot.py
@@ -9,11 +9,11 @@ player_color = (83, 83, 83)
 print('starting at 4 seconds...')
 time.sleep(4)
 
-X = 330.0
+X = 530.0
 
 def detect_object(x1, x2, y1, y2):
     screen = ImageGrab.grab()
-    bg_color = screen.getpixel((10, 80))
+    bg_color = screen.getpixel((180, 160))
     for x in range(int(x1), int(x2)):
         for y in range(y1, y2):
             color = screen.getpixel((x, y))
@@ -22,8 +22,9 @@ def detect_object(x1, x2, y1, y2):
                 return True
 
 while True:
-    if detect_object(X, X+20, 380, 420):
+    if detect_object(X, X+90, 590, 680):
         pyautogui.press('up')
         seconds = time.clock()
         # print('Time:', seconds)
-        X += 0.9
+        # X += 0.9
+        X += 1.0
